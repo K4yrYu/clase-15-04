c0ff6b4c05c12cea7bb084befe421b4294698c99
Vicente_Palma <163549177+K4yrYu@users.noreply.github.com>
2024-04-15 09:53:45 -0400

    actualizado ver2 (a la mala xd)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
 
 
 let nombre = document.getElementById("nombre");
+let apellido = document.getElementById("apellido");
 
 let btnENVIAR = document.getElementById("btnENVIAR");
 
-btnENVIAR.addEventListener("click", function(e)) {
-    console.log("hago click");
-}
+btnENVIAR.addEventListener("click", function(e){
+    e.preventDefault();
+    
+    nombre = nombre.value;
+    apellido = apellido.value;
+
+    req.push({
+        nombre,
+        apellido
+    });
+
+    console.log(`el nombre es: ${nombre}`);
+    console.log(`el nombre es: ${apellido}`);
+});
