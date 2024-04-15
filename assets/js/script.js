d416d568bd60cc2085dbd3330db3d922f111c3a5
vicente <vice.palma@duocuc.cl>
2024-04-15 09:39:36 -0400

    ejemplo de clases

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+
+
+let nombre = document.getElementById("nombre");
+
+let btnENVIAR = document.getElementById("btnENVIAR");
+
+btnENVIAR.addEventListener("click", function(e)) {
+    console.log("hago click");
+}
